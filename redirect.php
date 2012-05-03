656dc56b1ebfec63a6f557e8c51b85feb45e5840
Jin Young Hur <hurims@gmail.com>
2012-05-03 22:49:43 +0900

    testing

--- redirect.php
+++ redirect.php
@@ -1,5 +1,5 @@
 <?
-      header( 'Location: http://m.map.naver.com/map.nhn?isCurLoc=Y&lat=37.5666103&lng=126.9783882' ) ;
-      //header( 'Location: http:/hurims.dyndns.org/movies' ) ;
+      //header( 'Location: http://m.map.naver.com/map.nhn?isCurLoc=Y&lat=37.5666103&lng=126.9783882' ) ;
+      header( 'Location: http:/hurims.dyndns.org/movies' ) ;
 ?>
 
